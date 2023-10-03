35d1464ea98f013acfdfd5f6997f3ccb1d0f12b4
robert <roberts.shalajevs@gmail.com>
2023-10-03 22:20:03 +0300

    home page

---
+++ src/components/Header.vue
@@ -0,0 +1,107 @@
+<script setup>
+
+</script>
+
+<template>
+    <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    
+    <header>
+        <div class="navigator">
+            <div class="navigator2">
+                <a class="logo" href="/home">FitFuel</a>
+
+                <div class="routes">
+                <a href="/home">Home</a>
+                <a href="/product">Product</a>
+                <a href="/about">About</a>
+                </div>
+            </div>
+        </div>
+    </header>
+  
+</template>
+
+<style scoped>
+
+
+    template{
+        background: #222;
+                width: 1440px;
+        height: 74px;
+        flex-shrink: 0;
+    }
+
+    .Home{
+        color: #FFF;
+        display: flex;
+        width: 71px;
+        height: 23px;
+        font-size: 15px;
+        padding-right: 0px;
+        align-items: center;
+        flex-shrink: 0;
+    }
+
+    .Product{
+        color: #FFF;
+        font-family: Outfit;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 23px; /* 92% */
+    }
+    .About{
+        color: #FFF;
+        font-family: Outfit;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 23px; /* 92% */
+    }
+
+    header {
+         padding: 0 20px;
+      }
+
+      .navigator {
+         display: flex;
+         justify-content: flex-start;
+         align-items: center;
+      }
+
+
+    .navigator2 {
+         width: 40%;
+         display: flex;
+         justify-content: space-between;
+         align-items: center;
+         flex-direction: row;
+    }
+
+    .routes {
+        color: #FFF;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         gap:40px;
+         flex-direction: row;
+
+      }
+
+      a{
+        font-family: Outfit;
+        text-decoration: none;
+        outline: none;
+        color: inherit;
+      }
+
+      .logo{
+        color: #FFF;
+font-family: Outfit;
+font-size: 25px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+      }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,183 @@
+<template>
+    <link href='https://fonts.googleapis.com/css?family=Gochi Hand' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <p class="autumn">Autumn set</p>
+    <p class="high">HIGH PROTEIN</p>
+    <p class="sale">SALE</p>
+    <div class="point">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="100" viewBox="0 0 28 100" fill="none">
+            <rect width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="15.0002" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="30" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="44.9999" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="60.0001" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="75" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="15.0002" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="30" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="44.9999" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="60.0001" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="75" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="89.9998" width="9.33333" height="10" rx="4.66667" fill="white"/>
+        </svg>
+    </div>
+    <div class="center-container">
+        <a href="/product">
+            <button class="button">Shop now</button>
+        </a>
+    </div>
+
+
+    
+
+
+    <div class="point2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="100" viewBox="0 0 28 100" fill="none">
+            <rect width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="15.0002" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="30" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="44.9999" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="60.0001" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect y="75" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="15.0002" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="30" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="44.9999" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="60.0001" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="75" width="9.33333" height="10" rx="4.66667" fill="white"/>
+            <rect x="18.6667" y="89.9998" width="9.33333" height="10" rx="4.66667" fill="white"/>
+        </svg>
+    </div>
+
+    <div class="circle">
+        <img src="../components/protein.png" alt="Your Image">
+    </div>
+
+
+
+
+</template>
+
+<style scoped>
+
+.circle img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: cover;
+  margin-top: 150px;
+}
+.circle {
+  width: 548.453px;
+  height: 549.005px;
+  transform: rotate(0.5deg);
+  flex-shrink: 0;
+  border-radius: 50%;
+  background: #FFB800;
+  position: fixed;
+  top: 20px; /* Adjust the top position */
+  right: -150px; /* Adjust the right position */
+  margin-top: 60px; /* Adjust the top margin */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+
+.point2 {
+    width: 28px;
+    height: 100px;
+    transform: rotate(-90deg);
+    flex-shrink: 0;
+    position: fixed;
+    top: 30px; /* Adjust the top position */
+    right: 700px; /* Adjust the right position */
+}
+
+
+  .point {
+    width: 28px;
+    height: 100px;
+    flex-shrink: 0;
+    margin-left: 100px;
+  }
+
+  template {
+    background: #222;
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Центрирование по горизонтали */
+    justify-content: center; /* Центрирование по вертикали */
+    height: 100vh; /* Высота 100% видимой области */
+    margin: 0; /* Убираем отступы */
+  }
+
+  .autumn, .high, .sale {
+    transform: rotate(-10deg);
+    font-style: normal;
+    font-weight: bold; /* Жирный шрифт */
+    margin: 0;
+  }
+
+  .autumn {
+    color: #FFF;
+    font-family: Gochi Hand;
+    font-size: 40px;
+    margin-left: 470px;
+    margin-bottom: -25px;
+    margin-top: 200px; /* Отодвигаем немного вниз */
+  }
+
+  .high {
+    color: #FFB800;
+    font-family: Inter;
+    font-size: 100px; /* Уменьшен размер для смещения вправо */
+    margin-bottom: -20px; /* Немного опускаем надпись */
+    margin-left: 400px; /* Смещаем немного вправо */
+  }
+
+  .sale {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 80px; /* Уменьшен размер для смещения вправо */
+    margin-left: 700px;
+    margin-top: -40px; /* Отодвигаем немного вниз */
+  }
+
+
+.center-container {
+  text-align: center;
+}
+
+.button {
+  width: 187px;
+  height: 88px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  background: #FFB800;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  border: none;
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: bold;
+  color: black;
+}
+
+.button:hover {
+  background: #E5A500;
+}
+
+.button:active {
+  background: #D39C00;
+}
+
+.center-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 30vh;
+  margin: 0;
+}
+
+
+
+</style>
